565d15967a4571cf87f0d9987ab0eee2fca791df
SharmiJohn <1422000joselvi@gmail.com>
2024-07-12 12:40:20 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [value, setvalue] = useState("");
+  const [displayvalue, setdisplayvalue] = useState("");
+  const [data, setdata] = useState([
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." },
+  ]);
+  const handleClick = () => {
+    let captalize =value.charAt(0).toUpperCase()+value.slice(1)
+    
+  const getdata=data.find((datas)=>datas.word===captalize)
+  if(getdata){
+    setdisplayvalue(getdata.meaning)
+  }
+  else{
+    setdisplayvalue("Word not found in the dictionary.")
+  }
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+      <div style={{ display: "flex" }}>
+        <input
+          value={value}
+          placeholder="Search for a word..."
+          onChange={(e) => setvalue(e.target.value)}
+        />
+        <button onClick={() => handleClick()}>Search</button>
+      </div>
+      <h3>Definition:</h3>
+      <p>{displayvalue}</p>
     </div>
   );
 }
